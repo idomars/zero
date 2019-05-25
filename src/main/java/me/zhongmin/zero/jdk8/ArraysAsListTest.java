9f90d71950239ac40a7809522b4fa15edb1b92af
min <120081275@qq.com>
2019-05-25 19:57:05 +0800

    1

---
+++ src/main/java/me/zhongmin/zero/jdk8/ArraysAsListTest.java
@@ -0,0 +1,11 @@
+package me.zhongmin.zero.jdk8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysAsListTest {
+
+    public void test(){
+        List<Integer> list = Arrays.asList(1,2,3);
+    }
+}
